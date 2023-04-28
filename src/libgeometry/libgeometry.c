f6419b3188cb9ab2b41d5aebdf1d81f91e55743a
elfkkkkk <perminovaea04@mail.ru>
2023-04-29 00:49:30 +0700

    Add intersection

--- src/libgeometry/libgeometry.c
+++ src/libgeometry/libgeometry.c
@@ -235,9 +235,10 @@ Figure format_figure(Figure* fig)
                            * (halfp - third_side));
             return *fig;
 
-        } else {
-            printf("Non-degenerate triangle!\n");
-        }
+        } 
+        // else {
+        //     printf("Non-degenerate triangle!\n");
+        // }
     }
     if (fig->type == 0) {
         fig->squre = M_PI * SQR(fig->radius);
@@ -245,4 +246,214 @@ Figure format_figure(Figure* fig)
         return *fig;
     }
     return *fig;
+}
+
+void intersection_output(int figure_num, Figure *fig_obj)
+{
+    if (((fig_obj[1].squre > 0) && fig_obj[2].squre > 0)
+        && (fig_obj[3].squre > 0)) {
+        if ((fig_obj[1].squre > 0) && fig_obj[2].squre > 0) {
+            if (figure_intersection(&fig_obj[1], &fig_obj[2]) == 1) {
+                fig_obj[1].intersect[2] = 1;
+                fig_obj[2].intersect[1] = 1;
+            }
+        }
+
+        if ((fig_obj[2].squre > 0) && fig_obj[3].squre > 0) {
+            if (figure_intersection(&fig_obj[2], &fig_obj[3]) == 1) {
+                fig_obj[2].intersect[3] = 1;
+                fig_obj[3].intersect[2] = 1;
+            }
+        }
+
+        if ((fig_obj[1].squre > 0) && fig_obj[3].squre > 0)
+            if (figure_intersection(&fig_obj[1], &fig_obj[3]) == 1) {
+                {
+                    fig_obj[1].intersect[3] = 1;
+                    fig_obj[3].intersect[1] = 1;
+                }
+            }
+        printf("\n");
+        for (int i = 1; i < 4; i++) {
+            printf("Figure №%d intersections:\n", i);
+            for (int j = 0; j < 4; j++) {
+                if (fig_obj[i].intersect[j] == 1) {
+                    printf("figure №%d\n", j);
+                }
+            }
+            printf("\n");
+        }
+    }
+}
+
+void swap(double* xp, double* yp)
+{
+    double temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+int figure_intersection(Figure* fig, Figure* fig2)
+{
+    if ((fig->type == 0) && (fig2->type == 0)) {
+        double d = (fig->center.x - fig2->center.x)
+                        * (fig->center.x - fig2->center.x)
+                + (fig->center.y - fig2->center.y)
+                        * (fig->center.y - fig2->center.y);
+
+        if (d <= (fig->radius + fig2->radius) * (fig->radius + fig2->radius)
+            && d >= (fig->radius > fig2->radius ? fig->radius - fig2->radius
+                                                : fig2->radius - fig->radius))
+            return 1;
+        else
+            return 0;
+        ;
+    }
+
+    if ((fig->type == 1) && (fig2->type == 1)) {
+        int ff = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                ff = cross_segment(
+                        fig->p[i].x,
+                        fig->p[i].y,
+                        fig->p[i + 1].x,
+                        fig->p[i + 1].y,
+                        fig2->p[j].x,
+                        fig2->p[j].y,
+                        fig2->p[j + 1].x,
+                        fig2->p[j + 1].y);
+            }
+        }
+        if (ff) {
+            return 1;
+        }
+        return 0;
+    }
+
+    if (((fig->type == 0) && (fig2->type == 1))
+        || ((fig->type == 1) && (fig2->type == 0))) {
+        if (fig->type == 0) {
+            return tri_cir_intersection(fig, fig2);
+        }
+        if (fig->type == 1)
+            return tri_cir_intersection(fig2, fig);
+    }
+    return 0;
+}
+
+double s_n(Figure* fig, int n)
+{
+    double sum = 0;
+    for (int i = 0; i < n; ++i) {
+        double det = fig->p[i % n].x * fig->p[(i + 1) % n].y
+                - fig->p[(i + 1) % n].x * fig->p[i % n].y;
+        sum += det;
+    }
+    return fabs(sum / 2);
+}
+
+int tri_cir_intersection(Figure* fig, Figure* fig2)
+{
+    const double eps = 1e-10;
+    double x0 = fig->center.x;
+    double y0 = fig->center.y;
+    double r = fig->radius;
+    double x1, x2, y1, y2;
+    int track = 0;
+    for (int i = 0; i < 3; i++) {
+        x1 = fig2->p[i].x;
+        y1 = fig2->p[i].y;
+        x2 = fig2->p[i + 1].x;
+        y2 = fig2->p[i + 1].y;
+
+        double dx01 = x1 - x0, dy01 = y1 - y0, dx12 = x2 - x1, dy12 = y2 - y1;
+        double a = SQR(dx12) + SQR(dy12);
+        if (fabs(a) < eps) {
+            printf("Начало и конец отрезка совпадают!\n");
+            return 0;
+        }
+        double k = dx01 * dx12 + dy01 * dy12;
+        double c = SQR(dx01) + SQR(dy01) - SQR(r);
+        double d1 = SQR(k) - a * c;
+        if (d1 < 0)
+            printf("Прямая не пересекается с окружностью - отрезок снаружи\n");
+        else if (fabs(d1) < eps) {
+            double t = -k / a;
+            track++;
+            printf("Прямая касается окружности в точке\n");
+            if (t > 0 - eps && t < 1 + eps) {
+                track++;
+                printf("Отрезок снаружи, имеет общую точку с окружностью\n");
+            } else {
+                printf("Отрезок строго снаружи\n");
+            }
+        } else {
+            double t1 = (-k + sqrt(d1)) / a, t2 = (-k - sqrt(d1)) / a;
+            if (t1 > t2)
+                swap(&t1, &t2);
+
+            printf("Прямая пересекает окружность в двух точках\n");
+
+            if (t1 >= 0 - eps && t2 <= 1 + eps) {
+                if (t1 > 0 - eps && t2 < 1 + eps) {
+                    track++;
+                    printf("Отрезок строго внутри\n");
+                } else {
+                    printf("Отрезок внутри, имеет  как минимум одну общую "
+                           "точку с окружностью\n");
+                    track++;
+                }
+            }
+
+            else if ((t2 <= 0 + eps) || (t1 >= 1 - eps)) {
+                if (!(t2 < 0 + eps || t1 > 1 - eps)) {
+                    track++;
+                    printf("Отрезок строго снаружи\n");
+                } else {
+                    track++;
+                    printf("Отрезок снаружи, имеет общую точку с "
+                           "окружностью\n");
+                }
+
+            } else {
+                track++;
+                printf("Отрезок пересекает окружность в двух точках\n");
+            }
+        }
+    }
+    if (track > 0)
+        return 1;
+    return 0;
+}
+
+int cross_segment(
+        double x1,
+        double y1,
+        double x2,
+        double y2,
+        double x3,
+        double y3,
+        double x4,
+        double y4)
+{
+    double denum = (y4 - y3) * (x1 - x2) - (x4 - x3) * (y1 - y2);
+    int ff = 0;
+    if (denum == 0) {
+        if ((x1 * y2 - x2 * y1) * (x4 - x3) - (x3 * y4 - x4 * y3) * (x2 - x1)
+                    == 0
+            && (x1 * y2 - x2 * y1) * (y4 - y3) - (x3 * y4 - x4 * y3) * (y2 - y1)
+                    == 0)
+            ff = 1;
+        else
+            ff = 0;
+    } else {
+        double num_a = (x4 - x2) * (y4 - y3) - (x4 - x3) * (y4 - y2);
+        double num_b = (x1 - x2) * (y4 - y2) - (x4 - x2) * (y1 - y2);
+        double Ua = num_a / denum;
+        double Ub = num_b / denum;
+
+        ff = (Ua >= 0 && Ua <= 1 && Ub >= 0 && Ub <= 1 ? 1 : 0);
+    }
+    return ff;
 }
--- src/libgeometry/libgeometry.h
+++ src/libgeometry/libgeometry.h
@@ -26,5 +26,18 @@ int check_brackets(char input[], int len, int countStart, int answer_point);
 int converter(char input[], int len, int countStart, int answer_point, int figure_num, Figure *fig_obj);
 int output(char input[], int len, int countStart, int answer_point, int figure_num, Figure *fig_obj);
 Figure format_figure(Figure* fig);
+double s_n(Figure* fig, int n);
+int figure_intersection(Figure* fig, Figure* fig2);
+void intersection_output(int figure_num, Figure *fig_obj);
+int tri_cir_intersection(Figure* fig, Figure* fig2);
+int cross_segment(
+        double x1,
+        double y1,
+        double x2,
+        double y2,
+        double x3,
+        double y3,
+        double x4,
+        double y4);
 
 #endif
